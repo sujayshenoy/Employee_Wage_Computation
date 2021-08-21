53751810de7f097455cd60f15ba1483e25a0eda3
Sujay S Shenoy <sujay.shenoy@ymedialabs.com>
2021-08-21 13:45:29 +0530

    Added method to find wage of a regular employee

--- src/main_package/Main.java
+++ src/main_package/Main.java
@@ -1,4 +1,5 @@
 package main_package;
+import java.util.*;
 
 public class Main {
 
@@ -6,12 +7,15 @@ public class Main {
 		
 		System.out.println("Welcome to Employee Wage Computation");
 		
-		Employee e = new Employee();
+		Employee e = new Employee(20); //20 because wage for that particular employee is 20 per hour
 		if(e.isPresent()) {
 			System.out.println("Employee is Present");
 		}
 		else {
 			System.out.println("Employee is Absent");
-		}		
+		}	
+		
+		double wage = e.calculateRegularWage();
+		System.out.println("Regular Employee Wage = "+wage);
 	}	
 }
