00ef8857ec5fb78c48c2fa5672e4a37acc8d4d7e
Sujay S Shenoy <sujay.shenoy@ymedialabs.com>
2021-09-07 00:02:30 +0530

    Added method to get total wage of companies by company name

--- src/main_package/Main.java
+++ src/main_package/Main.java
@@ -1,18 +1,17 @@
 package main_package;
 
-import java.util.ArrayList;
-import java.util.List;
+import java.util.Map;
+import java.util.HashMap;
 
 public class Main {
+	static Map<String, Company> companies = new HashMap<String, Company>();
 	public static void main(String[] args) {
-		List<Company> companies = new ArrayList<Company>();
-
 		System.out.println("Welcome to Employee Wage Computation");
-		
-		companies.add(new Company("Jio", 20, 100));
-		companies.add(new Company("Dmart", 25, 150));
 
-		for (Company company : companies) {
+		companies.put("Jio", new Company("Jio", 20, 100));
+		companies.put("Dmart", new Company("Dmart", 25, 150));
+
+		for (Company company : companies.values()) {
 			System.out.println("Company " + company.getCompanyName());
 			company.createEmployee(1);
 			company.createEmployee(2);
@@ -22,10 +21,19 @@ public class Main {
 				System.out.println("Total wage of EMP: " + employee.empId + " is "
 						+ String.format("%.2f", company.calculateWage(employee)));
 			}
-			
-			System.out.println("Daily Wage Cost to Company: "+String.format("%.2f",company.getDailyWage()));
-			System.out.println("Total Wage of Company: "+company.getCompanyName()+" is "+String.format("%.2f", company.getTotalEmpWage()));
+
+			System.out.println("Daily Wage Cost to Company: " + String.format("%.2f", company.getDailyWage()));
+			System.out.println("Total Wage of Company: " + company.getCompanyName() + " is "
+					+ String.format("%.2f", company.getTotalEmpWage()));
 			System.out.println("");
 		}
-	}	
+
+		//Print Total wage of Jio
+		System.out.println("Total Wage for Jio: "+String.format("%.2f",getTotalWageByCompany("Jio")));
+	}
+	
+	private static double getTotalWageByCompany(String companyName){
+		Company company = companies.get(companyName);
+		return company.getTotalEmpWage();
+	}
 }
